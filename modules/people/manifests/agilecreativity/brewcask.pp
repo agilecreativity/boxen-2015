bb6ee14d28f03ce0d0c0874c0f5d6f5b36969cca
Burin Choomnuan <agilecreativity@gmail.com>
2015-07-26 11:29:16 +1000

    Add calibre and ffmpeg package

--- modules/people/manifests/agilecreativity/brewcask.pp
+++ modules/people/manifests/agilecreativity/brewcask.pp
@@ -3,12 +3,13 @@ class people::agilecreativity::brewcask {
   $env = {
     packages => {
       brew   => [
-        # '4k-youtube-to-mp3',
-        # '4k-video-downloader',
+        'calibre',
         'grandperspective',
+        'kindle',
+        'kindle-previewer',
         'vlc',
-        'vlcstreamer'
-        #'ffmpegx'
+        'vlcstreamer',
+        'ffmpegx'
         ## Note: this will install version 3.0.x, need to comment out for older version of mongo
         # 'mongodb',
         # 'robomongo'
